edb1c2eb36afc00a35c93d3189915cb1c399781e
Радмир Бахтияров <axxxy008@mail.ru>
2023-02-01 11:29:48 +0300

    wb-53Project

---
+++ wiren/wiren/asgi.py
@@ -0,0 +1,15 @@
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from console.routing import ws_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiren.settings')
+
+application = ProtocolTypeRouter({
+	'http': get_asgi_application(),
+	'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+})
+
